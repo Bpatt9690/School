1718148e4b295daa5ed9ba9071f40a873ecfac71
Blake Patterson <patte480@knights.ucf.edu>
2019-10-20 18:21:52 -0400

    Homework 3

---
+++ binaryTree.java
@@ -0,0 +1,36 @@
+import java.util.*
+
+
+public class binaryTree
+{
+	public static void main(String[] args)
+	{
+
+		System.out.println("Hey");
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
