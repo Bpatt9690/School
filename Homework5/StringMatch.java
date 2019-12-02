f036784801e0740db219eb3cc44cbd26b7c4d5da
Blake Patterson <patte480@knights.ucf.edu>
2019-12-02 10:31:14 -0500

    Branching from here

---
+++ Homework5/StringMatch.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class StringMatch{
+
+	public static int[] CheckStringMatch(char[] text_arr, char[] fragment_arr){
+
+	int[] test = new int[3];
+
+	int[] nullArray = new int[1];
+
+	if(text_arr == null || fragment_arr == null)
+		return nullArray;
+
+
+	return test;
+
+	}
+	
+}
